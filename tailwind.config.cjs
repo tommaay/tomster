4a8a70f81252d2efce414555bbf9c0f05a4f1f59
tommaay <tommy@tomster.me>
2023-10-04 15:08:47 -0700

    Basic portfolio site for the time being

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,10 @@ module.exports = {
       colors: {
         dark: "#151C3A",
         base: "#222222",
+        "primary-1": "#f8bf99",
+        "primary-2": "#fcab76",
+        "primary-3": "#f7c29f",
+        button: "#fcab76",
       },
     },
   },
